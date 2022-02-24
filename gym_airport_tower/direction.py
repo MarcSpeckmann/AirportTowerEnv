b2f1e773f192e0930c406610a96df6867063852b
Marc Speckmann <marc-speckmann@web.de>
2022-02-24 11:18:44 +0100

    docstring for enums

--- gym_airport_tower/direction.py
+++ gym_airport_tower/direction.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class Direction(Enum):
+    """
+    This Enum class describes the direction the planes are moving to. The directions are the four Cardinal directions.
+    """
     NORTH = 0
     EAST = 1
     SOUTH = 2
--- gym_airport_tower/runway_alignment.py
+++ gym_airport_tower/runway_alignment.py
@@ -2,5 +2,8 @@ from enum import Enum
 
 
 class RunwayAlignment(Enum):
+    """
+    This Enum class describes the orientation of runways. Either the runway is aligned horizontal or vertical.
+    """
     HORIZONTAL = 0
     VERTICAL = 1
