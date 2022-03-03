59ef04cf17ea5b7e491b5cb852789eb21bddfe9e
Marc Speckmann <marc-speckmann@web.de>
2022-03-03 15:44:33 +0100

    more ram

--- slurm_single_agent.sh
+++ slurm_single_agent.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=100
 #SBATCH --time=05:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=100G
 # node the job ran on
 echo "Job ran on:" $(hostname)
 # load the relevant modules
